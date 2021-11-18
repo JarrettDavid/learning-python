e8a310f2c9cf2a26159c121d90b0c9b6758c750f
Jarrett David <14225981+JarrettDavid@users.noreply.github.com>
2021-11-17 20:23:13 -0600

    Add project files.

---
+++ Ch2/helloworld_start.py
@@ -0,0 +1,27 @@
+#
+# Example file for HelloWorld
+#
+
+#
+# Declaring my first Python function
+# This function uses the input function
+# to read information typed in by the
+# user. It also uses the print command
+# which prints information out to the 
+# terminal
+#
+def main():
+    name = input("What is your name? ");
+    followUp = "Hello world, introducing... ";
+    print(followUp+name);
+    print("Nice to meet you, ",name);
+
+#
+#
+# To limit the execution of functions only to when they are called
+# we use the below line to find the function name (__main__) within 
+# the file. If it exists then execute the function
+#
+#
+if __name__ == "__main__":
+    main();
---
+++ Ch2/loops_start.py
@@ -0,0 +1,41 @@
+#
+# Example file for working with loops
+#
+
+def main():
+  x = 0
+
+  # define a while loop
+  #while(x < 5):
+  #    print(x)
+  #    x+=1
+
+  # define a for loop
+  for x in range(0, 5):
+       print(x)
+       x+=1
+
+  # use a for loop over a collection
+  days=["Mon","Tues","Wed","Thur","Fri","Sat","Sun"]
+  for d in days:
+      print(d)
+  
+  # use the break and continue statements
+  for x in range(5,10):
+      if(x==7): break
+      print(x)
+   #continue will skip over whatever satisfies the condition staement
+   #in this case 6 and 8 are skipped while 5, 7 and 9 are printed
+  for x in range(5,10):
+      if(x % 2 == 0): continue
+      print(x)
+  #using the enumerate() function to get index 
+  days=["Mon","Tues","Wed","Thur","Fri","Sat","Sun"]
+  for i,d in enumerate(days):
+      #if(i % 2 == 0): continue
+      #print(i,'.',d)
+      if(i == 5): break
+      print(i,'.',d)
+  
+if __name__ == "__main__":
+  main()
